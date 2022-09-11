8833013c700079f7c3b3c8ca6593a305001d3f7d
DimaSurdiy <dsurdiy@ukr.net>
2022-09-12 00:16:22 +0300

    add contacts and filter slices

--- src/redux/contactsActions.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const add = createAction('contacts/add');
-export const remove = createAction('contacts/remove');
-export const changeFilter = createAction('contacts/filter');
